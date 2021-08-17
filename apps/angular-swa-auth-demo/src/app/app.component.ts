c537af4716547fe7e96f690d5f20b679f0920e99
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-08-17 11:53:45 +0100

    initial commit

---
+++ apps/angular-swa-auth-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ccacca-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-swa-auth-demo';
+}
