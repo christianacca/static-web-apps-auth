5ff8c0f500f26c508054048464e10bd043f3e5cc
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-08-18 20:00:14 +0100

    Move staticwebapp.config.json

--- apps/angular-swa-auth-demo/src/app/products/products.component.ts
+++ apps/angular-swa-auth-demo/src/app/products/products.component.ts
@@ -7,7 +7,7 @@ import { ProductService } from './product.service';
   selector: 'app-products',
   template: `
     <div class="content-container">
-      <app-list-header title="Products" (add)="enableAddMode()" (refresh)="getProducts()"></app-list-header>
+      <app-list-header title="Product(s)" (add)="enableAddMode()" (refresh)="getProducts()"></app-list-header>
       <div class="columns is-multiline is-variable">
         <div class="column is-8" *ngIf="products$ | async as products">
           <app-product-list
