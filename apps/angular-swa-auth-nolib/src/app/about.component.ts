be8104b89845c09834ebbe95f7eb63e39aed5f4b
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-08-21 14:23:01 +0100

    better config and docs for nolib app demo

--- apps/angular-swa-auth-nolib/src/app/about.component.ts
+++ apps/angular-swa-auth-nolib/src/app/about.component.ts
@@ -5,16 +5,27 @@ import { Component } from '@angular/core';
   template: `
     <div class="content-container">
       <div class="content-title-group not-found">
-        <h2 class="title">Product Wish List</h2>
+        <h2 class="title">Overview</h2>
         <p>
-          This project was created to help represent a fundamental app written with Angular. The shopping theme is used
-          throughout the app.
+          This project is based on the
+          <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp-authentication/tree/main/angular-app"
+            >MS official sample app</a
+          >.
+        </p>
+        <p>
+          It shows an example app that does NOT require the <code>angular-swa-auth</code> helper library to implement
+          authentication. This is typically where you need to serve your app to authentication users only.
         </p>
         <br />
         <h2 class="title">Resources</h2>
         <ul>
           <li>
-            <a href="https://github.com/MicrosoftDocs/mslearn-staticwebapp"> Code in GitHub </a>
+            <a href="https://github.com/christianacca/static-web-apps-auth/tree/master/apps/angular-swa-auth-nolib">
+              Code in GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://angular-swa-auth-nolib.codingdemo.co.uk"> Deployed Demo site </a>
           </li>
         </ul>
       </div>
