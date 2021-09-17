31f789704aa561fd5c82d696e062c8aee1a30f9f
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-09-17 17:56:05 +0100

    docs: more jsdoc comments

--- libs/angular-swa-auth/src/lib/auth-event.ts
+++ libs/angular-swa-auth/src/lib/auth-event.ts
@@ -1,11 +1,21 @@
 import { ClientPrincipal } from './client-principal';
 
+/**
+ * Type names of the authentication session events
+ */
 export type AuthEventType = 'login' | 'sign-up' | 'logout' | 'purge';
 
+/**
+ * An authentication session event
+ */
 export abstract class AuthEvent {
   abstract type: AuthEventType;
   abstract user: ClientPrincipal;
 
+  /**
+   * Creates an event object representing the user having logged in
+   * @param user The current user
+   */
   static login(user: ClientPrincipal): AuthEvent {
     return {
       type: 'login',
@@ -13,6 +23,10 @@ export abstract class AuthEvent {
     };
   }
 
+  /**
+   * Creates an event object representing the user having logged out
+   * @param user The current user
+   */
   static logout(user: ClientPrincipal): AuthEvent {
     return {
       type: 'logout',
@@ -20,6 +34,10 @@ export abstract class AuthEvent {
     };
   }
 
+  /**
+   * Creates an event object representing the user requesting to "forget me"
+   * @param user The current user
+   */
   static purge(user: ClientPrincipal): AuthEvent {
     return {
       type: 'purge',
@@ -27,6 +45,10 @@ export abstract class AuthEvent {
     };
   }
 
+  /**
+   * Creates an event object representing having signed up
+   * @param user The current user
+   */
   static signUp(user: ClientPrincipal): AuthEvent {
     return {
       type: 'sign-up',
