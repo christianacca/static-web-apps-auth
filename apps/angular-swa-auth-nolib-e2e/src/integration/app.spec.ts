d3f9dc7c14a8365b5fb40c8cb342a1d6dc4e6002
Christian Crowhurst <christian.crowhurst@gmail.com>
2021-10-17 08:11:02 +0100

    chore: integrate with cypress dashboard

--- apps/angular-swa-auth-nolib-e2e/src/integration/app.spec.ts
+++ apps/angular-swa-auth-nolib-e2e/src/integration/app.spec.ts
@@ -1,14 +1,9 @@
-import { getGreeting } from '../support/app.po';
-
 describe('angular-swa-auth-nolib', () => {
+  // todo: use cy.session api to perform login once per fixture file(s)
+
   beforeEach(() => cy.visit('/'));
 
   it('should display login page', () => {
-    // Custom command example, see `../support/commands.ts` file
-    // cy.login('my-email@something.com', 'myPassword');
-
-    // todo: use new cy.session api to perform login once per fixture file
-
     cy.url().should('include', '/login');
   });
 });
