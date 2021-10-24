e4389ec5a2fb1c2f390289292b332f7fe82fefbc
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-10-24 08:19:54 +0100

    Add testing-library/cypress

--- apps/angular-swa-auth-demo-e2e/src/integration/app.spec.ts
+++ apps/angular-swa-auth-demo-e2e/src/integration/app.spec.ts
@@ -1,9 +1,13 @@
+import * as po from '../support/pages/index.po';
+
 describe('angular-swa-auth-demo', () => {
   // todo: use cy.session api to perform login once per fixture file(s)
 
-  beforeEach(() => cy.visit('/'));
+  beforeEach(po.visit);
 
   it('should display about page', () => {
     cy.url().should('include', '/about');
+    cy.findByRole('heading', { name: 'Overview' }).should('exist');
+    cy.findByRole('heading', { name: 'Resources' }).should('exist');
   });
 });
--- apps/angular-swa-auth-demo-e2e/src/support/add-commands.ts
+++ apps/angular-swa-auth-demo-e2e/src/support/add-commands.ts
@@ -8,18 +8,21 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 
-// eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace Cypress {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Chainable<Subject> {
-    login(email: string, password: string): void;
+import '@testing-library/cypress/add-commands';
+import { login } from './commands';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable<Subject> {
+      login: typeof login;
+    }
   }
 }
 //
 // -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
-});
+Cypress.Commands.add('login', login);
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
