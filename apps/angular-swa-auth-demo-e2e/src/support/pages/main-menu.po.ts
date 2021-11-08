b69328695596305d73aef962d451603cc122998b
Christian Crowhurst <christian.crowhurst@mrisoftware.com>
2021-11-08 09:57:49 +0000

    tests: more integration tests

--- apps/angular-swa-auth-demo-e2e/src/support/pages/main-menu.po.ts
+++ apps/angular-swa-auth-demo-e2e/src/support/pages/main-menu.po.ts
@@ -2,6 +2,7 @@ export const component = () => cy.findByRole('navigation', { name: 'Main menu' }
 export const adminAreaMenuItem = () => component().findByRole('link', { name: 'Admin Area' });
 export const loginMenuItem = () => component().findByTestId('login');
 export const logoutMenuItem = () => component().findByRole('button', { name: 'Logout' });
+export const purgeMenuItem = () => component().findByRole('button', { name: 'Forget me' });
 export const loginWithGitHubMenuItem = () => component().findByTestId('login-with-github');
 export const offersMenuItem = () => component().findByRole('link', { name: 'Offers' });
 export const productsMenuItem = () => component().findByRole('link', { name: 'Products' });
